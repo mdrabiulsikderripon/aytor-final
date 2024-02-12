1054c4131c96f5dbc264ee8b4ea05b48f597dec3
MD RABIUL SIKIDER RIPON <149536081+mdrabiulsikderripon@users.noreply.github.com>
2024-02-12 21:38:23 +0600

    app.js

--- js/app.js
+++ js/app.js
@@ -249,6 +249,30 @@ $(function () {
         autoplay: true,
         dots: true,
         dotsClass: "cdots",
+                responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 
     });
 
