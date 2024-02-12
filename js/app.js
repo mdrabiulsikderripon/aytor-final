5626cfbb6de221d5344a31d93316dc51cd7ff4f1
MD RABIUL SIKIDER RIPON <149536081+mdrabiulsikderripon@users.noreply.github.com>
2024-02-13 01:27:54 +0600

    app.js

--- js/app.js
+++ js/app.js
@@ -19,6 +19,41 @@ $(document).ready(function () {
     });
 
 
+    // * INSTAGRAM SLIDER
+    
+    $('.instagramslider').slick({
+        slidesToShow: 6,
+        speed: 500,
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        dotsClass: "imdots",
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+    });
+
 });
 
 // * PRELOADER STARTS HERE
@@ -274,40 +309,6 @@ $(function () {
         }
     ]
 
-    });
-    // * INSTAGRAM SLIDER
-    
-    $('.instagramslider').slick({
-        slidesToShow: 6,
-        speed: 500,
-        arrows: false,
-        autoplay: true,
-        dots: true,
-        dotsClass: "imdots",
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
     });
 
     // * SHOP SLIDER
