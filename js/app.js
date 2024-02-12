ec1a5fedf637ae06ba0a60898bdc451fcc06528c
MD RABIUL SIKIDER RIPON <149536081+mdrabiulsikderripon@users.noreply.github.com>
2024-02-12 21:48:34 +0600

    app.js

--- js/app.js
+++ js/app.js
@@ -285,45 +285,29 @@ $(function () {
         dots: true,
         dotsClass: "imdots",
         responsive: [{
-                breakpoint: 1199,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1
-
-                }
-            },
-            {
-                breakpoint: 776,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
             }
-        ]
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
     });
 
     // * SHOP SLIDER
