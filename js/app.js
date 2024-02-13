d0610725cefbc8bc424cbb0e2d7a9ab53e254399
MD RABIUL SIKIDER RIPON <149536081+mdrabiulsikderripon@users.noreply.github.com>
2024-02-13 23:04:22 +0600

    app.js

--- js/app.js
+++ js/app.js
@@ -18,117 +18,6 @@ $(document).ready(function () {
         $currentSubmenu.slideToggle();
     });
 
-    // * COUNTER UP
-    $('.counter').counterUp({
-        delay: 10,
-        time: 5000,
-    });
-
-    // * LATEST NEWS SLIDER
-    $('.latestcardslider').slick({
-        slidesToShow: 4,
-        speed: 500,
-        arrows: false,
-        autoplay: true,
-        dots: true,
-        dotsClass: "lndots",
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
-
-    
-    // * CUSTOMER SLIDER
-    $('.customerslider').slick({
-        slidesToShow: 1,
-        speed: 500,
-        arrows: false,
-        autoplay: true,
-        dots: true,
-        dotsClass: "cdots",
-                responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-
-    });
-    
-    // * INSTAGRAM SLIDER
-    
-    $('.instagramslider').slick({
-        slidesToShow: 6,
-        speed: 500,
-        arrows: false,
-        autoplay: true,
-        dots: true,
-        dotsClass: "imdots",
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-    });
-
 });
 
 // * PRELOADER STARTS HERE
@@ -203,6 +92,13 @@ $(function () {
         autoplay: true,
     });
 
+    // * AOS 
+    AOS.init();
+
+    // * TOOLTIPS
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    
     // * PRODUCT SLIDER
     $('.productslider').slick({
         slidesToShow: 4,
@@ -235,13 +131,6 @@ $(function () {
         ]
     });
 
-    // * AOS 
-    AOS.init();
-
-    // * TOOLTIPS
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
     // * FINAL COUNTDOWN
     $(".timer").countdown("2025/01/01", function (event) {
         $(".days").text(event.strftime('%D'));
@@ -249,7 +138,7 @@ $(function () {
         $(".mins").text(event.strftime('%M'));
         $(".secs").text(event.strftime('%S'));
     });
-
+    
     // * DEAL SLIDER
     $('.dealslider').slick({
         slidesToShow: 2,
@@ -275,6 +164,46 @@ $(function () {
             }
         ]
 
+    });
+
+    // * LATEST NEWS SLIDER
+    $('.latestcardslider').slick({
+        slidesToShow: 4,
+        speed: 500,
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        dotsClass: "lndots",
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    // * COUNTER UP
+    $('.counter').counterUp({
+        delay: 10,
+        time: 5000,
     });
 
     // * TEAM CARD SLIDER
@@ -311,7 +240,75 @@ $(function () {
             }
         ]
     });
+  
+    // * CUSTOMER SLIDER
+    $('.customerslider').slick({
+        slidesToShow: 1,
+        speed: 500,
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        dotsClass: "cdots",
+                responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 
+    });
+    
+    // * INSTAGRAM SLIDER
+    $('.instagramslider').slick({
+        slidesToShow: 6,
+        speed: 500,
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        dotsClass: "imdots",
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+    });
 
     // * SHOP SLIDER
     $('.topslider').slick({
